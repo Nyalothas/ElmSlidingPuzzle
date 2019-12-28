d2bdf1088fca6fd8ab48000b4a5cf085890f87d8
Ny'alotha <nyalothas@gmail.com>
2019-12-28 04:43:44 +0200

    Added build and updated readme

--- README.md
+++ README.md
@@ -2,3 +2,10 @@
 ElmSlidingPuzzle
 
 So clearly in view... Or is it maybe a trick of the light?
+
+## Setup
+You need to install first elm create app then run the command
+
+```
+elm-app start
+```
---
+++ build/precache-manifest.38415109739f1d8df1356a49d84c595b.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/favicon.ico"
+  },
+  {
+    "revision": "d97e90d313144becf6be1c640d2d4bab",
+    "url": "/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/manifest.json"
+  },
+  {
+    "revision": "4a1ed777f770f980ca2a",
+    "url": "/static/css/main.8c3c60ca.chunk.css"
+  },
+  {
+    "revision": "4a1ed777f770f980ca2a",
+    "url": "/static/js/main.4a1ed777.chunk.js"
+  },
+  {
+    "revision": "1b922744246c39b6bc43",
+    "url": "/static/js/runtime~main.1b922744.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
