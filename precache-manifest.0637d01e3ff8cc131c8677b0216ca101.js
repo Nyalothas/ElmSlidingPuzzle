9fe190e90763b80b267fd4a734d79273d4685923
Ny'alotha <nyalothas@gmail.com>
2019-12-28 05:58:04 +0200

    Updates

---
+++ precache-manifest.0637d01e3ff8cc131c8677b0216ca101.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "987da976a8b2a4169346a497ba84749d",
+    "url": "/ElmSlidingPuzzle/favicon.ico"
+  },
+  {
+    "revision": "38068826676c3885f0bed4a8c86de643",
+    "url": "/ElmSlidingPuzzle/index.html"
+  },
+  {
+    "revision": "6ead519905b7b907c00965be37dd5cd7",
+    "url": "/ElmSlidingPuzzle/logo.svg"
+  },
+  {
+    "revision": "1c82903c2994c2507b385b9042bbfac5",
+    "url": "/ElmSlidingPuzzle/manifest.json"
+  },
+  {
+    "revision": "31049f82378690b694b0",
+    "url": "/ElmSlidingPuzzle/static/css/main.8c3c60ca.chunk.css"
+  },
+  {
+    "revision": "31049f82378690b694b0",
+    "url": "/ElmSlidingPuzzle/static/js/main.31049f82.chunk.js"
+  },
+  {
+    "revision": "8f6dfd489eee4d319930",
+    "url": "/ElmSlidingPuzzle/static/js/runtime~main.8f6dfd48.js"
+  },
+  {
+    "revision": "3ca81432027bbb7b376c",
+    "url": "/ElmSlidingPuzzle/static/js/vendors~main.3ca81432.chunk.js"
+  }
+]);
